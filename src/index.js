cee1b855b788afcfa051ba4ab50b1cccb0f339ef
Vishal Sutariya <SVishuS@1998>
2022-04-30 13:17:23 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCQt5WSaR4SJ8EPX3p_M6r09Yz4L6chxc0';
 
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (data) => {
+            this.setState({ videos: data });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
