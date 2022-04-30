f0cff5b8ea8f26b33e533e8d27a93f85f1b4dd04
Vishal Sutariya <SVishuS@1998>
2022-04-30 11:34:38 +0530

    state implemented

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCQt5WSaR4SJ8EPX3p_M6r09Yz4L6chxc0';
+
 const App = () => {
-    return <div>Hello World!</div>
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    );
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
